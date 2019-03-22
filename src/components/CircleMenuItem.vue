c2beaba42c4cfd9ff53099a0919b630e8cb3c465
leegeunhyeok <lghlove0509@naver.com>
2019-03-23 00:11:43 +0900

    ADD: CircleMenuItem styles

--- src/components/CircleMenuItem.vue
+++ src/components/CircleMenuItem.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="circle-menu-item">
+  <div class="circle-menu-item" ref="menu">
 
   </div>
 </template>
 
 <script>
 export default {
-  name: 'circle-menu-item'
+  name: 'circle-menu-item',
+  props: {
+    index: {
+      type: Number,
+      required: true
+    }
+  },
+  created () {
+    console.log(this.index)
+  },
+  mounted () {
+    let rotateDegree = this.index * 60
+    this.$refs.menu.style.transform = `rotate(${ rotateDegree }deg) skew(30deg)`
+  }
 }
 </script>
 
 <style lang="scss">
 
 .circle-menu-item {
-
+  position: absolute;
+  background-color: dodgerblue;
+  width: 200px;
+  height: 200px;
+  transform-origin: 100% 100%;
+  text-align: center;
+  line-height: 300px;
 }
 </style>
