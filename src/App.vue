00be0d5dcfa107f365223c4ee419f9c14ec5dadd
leegeunhyeok <lghlove0509@naver.com>
2019-03-22 23:51:33 +0900

    ADD: Degree calculate function

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="app" ref="app">
+    <CirclePanel :degree="degree"/>
   </div>
 </template>
 
 <script>
+import CirclePanel from '@/components/CirclePanel';
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    CirclePanel
+  },
+  data () {
+    return {
+      speed: 1,
+      screenWidth: 0,
+      degree: 0
+    }
+  },
+  created () {
+    this.speed = 1
+  },
+  mounted () {
+    this.screenWidth = window.innerWidth 
+        || document.documentElement.clientWidth
+        || document.body.clientWidth
+    this.$refs.app.addEventListener('mousemove', this.changeDegree)
+    this.$refs.app.addEventListener('touchmove', this.changeDegree)
+  },
+  methods: {
+    changeDegree (event) {
+      let x = event.clientX
+      let percent = x / (this.screenWidth / 2)
+      this.degree = 360 * percent * (this.speed * 0.5)
+    }
+  }
 }
 </script>
 
@@ -33,11 +62,13 @@ html, body {
 }
 
 #app {
+  position: relative;
   font-family: 'NanumSquareRound', 'Avenir', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background: linear-gradient(0deg, #74ebd5, #acb6e5);
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(0deg, #acb6e5, #74ebd5);
+  overflow: hidden;
 }
 </style>
