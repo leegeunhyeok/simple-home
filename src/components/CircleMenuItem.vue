dae75afce30d330d88a514e897417ddf2682ca88
leegeunhyeok <lghlove0509@naver.com>
2019-03-22 23:55:40 +0900

    ADD: CircleMenuItem.vue

---
+++ src/components/CircleMenuItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="circle-menu-item">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'circle-menu-item'
+}
+</script>
+
+<style lang="scss">
+
+.circle-menu-item {
+
+}
+</style>
