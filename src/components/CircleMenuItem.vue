7fada119f1dbbdbc3aad05947bfa61b82665e3e7
leegeunhyeok <lghlove0509@naver.com>
2019-03-24 10:49:56 +0900

    ADD: transition style

--- src/components/CircleMenuItem.vue
+++ src/components/CircleMenuItem.vue
@@ -33,5 +33,9 @@ export default {
   transform-origin: 100% 100%;
   text-align: center;
   line-height: 300px;
+  -webkit-transition: .3s;
+     -moz-transition: .3s;
+       -o-transition: .3s;
+          transition: .3s;
 }
 </style>
