dcce21b650c1c0f0506343158cb85aef45bcf06d
leegeunhyeok <lghlove0509@naver.com>
2019-03-27 12:44:47 +0900

    UPDATE: If no userdata, save default data to localstoreage

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,9 +1,25 @@
 
 const _VERSION = 'v1'
+const DEFAULT_DATA = {
+  menu: [
+    { text: 'a' },
+    { text: 'b' },
+    { text: 'c' },
+    { text: 'd' },
+    { text: 'e' },
+    { text: 'f' }
+  ]
+}
 
-const GET_USER_DATA = ({ commit }) => {
+const GET_USER_DATA = ({ commit, dispatch }) => {
   try {
-    commit('SET_USER_DATA', JSON.parse(localStorage.getItem(_VERSION)))
+    let userData = JSON.parse(localStorage.getItem(_VERSION))
+    if (userData) {
+      commit('SET_USER_DATA', userData)
+    } else {
+      commit('SET_USER_DATA', DEFAULT_DATA)
+      dispatch('SET_USER_DATA')
+    }
   } catch (e) {
     console.error(e)
     commit('SET_USER_DATA', {})
