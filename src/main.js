71fe83db59ace0f3ac1ec983207a26e0ac5c21c4
leegeunhyeok <lghlove0509@naver.com>
2019-03-27 13:41:53 +0900

    ADD: Font-awesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,112 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store/index'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faUser,
+  faUserShield,
+  faHome,
+  faMusic,
+  faPlay,
+  faPlayCircle,
+  faCat,
+  faDog,
+  faLocationArrow,
+  faMapMarkedAlt,
+  faGlobeAmericas,
+  faGlobe,
+  faStickyNote,
+  faImage,
+  faCamera,
+  faBuilding,
+  faSchool,
+  faBookmark,
+  faBookOpen,
+  faEye,
+  faUtensils,
+  faUserFriends,
+  faCode,
+  faTerminal,
+  faDatabase,
+  faBaby,
+  faLock
+} from '@fortawesome/free-solid-svg-icons'
+
+import {
+  faGithub,
+  faGitlab,
+  faFacebook,
+  faFacebookMessenger,
+  faTwitter,
+  faGoogle,
+  faYoutube,
+  faTwitch,
+  faInstagram,
+  faApple,
+  faBlogger,
+  faAndroid,
+  faJs,
+  faLinux,
+  faSteam,
+  faStackOverflow,
+  faReddit,
+  faTelegram,
+  faSkype,
+  faSlack
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add([
+  faUser,
+  faUserShield,
+  faHome,
+  faMusic,
+  faPlay,
+  faPlayCircle,
+  faCat,
+  faDog,
+  faLocationArrow,
+  faMapMarkedAlt,
+  faGlobeAmericas,
+  faGlobe,
+  faStickyNote,
+  faImage,
+  faCamera,
+  faBuilding,
+  faSchool,
+  faBookmark,
+  faBookOpen,
+  faEye,
+  faUtensils,
+  faUserFriends,
+  faCode,
+  faTerminal,
+  faDatabase,
+  faBaby,
+  faLock,
+  faGithub,
+  faGitlab,
+  faFacebook,
+  faFacebookMessenger,
+  faTwitter,
+  faGoogle,
+  faYoutube,
+  faTwitch,
+  faInstagram,
+  faApple,
+  faBlogger,
+  faAndroid,
+  faJs,
+  faLinux,
+  faSteam,
+  faStackOverflow,
+  faReddit,
+  faTelegram,
+  faSkype,
+  faSlack
+])
+Vue.component('fa-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
