38587da695f8bd6c037db7cc91c74583171bf02e
leegeunhyeok <lghlove0509@naver.com>
2019-03-27 11:15:48 +0900

    ADD: selectedMenuIndex

--- src/store.js
+++ src/store.js
@@ -5,11 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    screenWidth: 0
+    screenWidth: 0,
+    selectedMenuIndex: 0
   },
   mutations: {
     SET_WIDTH (state, width) {
       state.screenWidth = width
+    },
+    SET_SELECTED_MENU (state, index) {
+      state.selectedMenuIndex = index
     }
   },
   actions: {
