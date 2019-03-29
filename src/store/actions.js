f201cd94ca232d1bc161b0b14db249f08bc9c3cb
leegeunhyeok <lghlove0509@naver.com>
2019-03-29 10:23:43 +0900

    DOCS: Add comment (actions.js)

--- src/store/actions.js
+++ src/store/actions.js
@@ -95,25 +95,31 @@ const DEFAULT_DATA = {
 
 const GET_USER_DATA = ({ commit, dispatch }) => {
   try {
+    // Get localStorage value and parse to JSON data
     let userData = JSON.parse(localStorage.getItem(_VERSION))
     if (userData) {
       commit('SET_USER_DATA', userData)
     } else {
+      // Set default user data and save to localStorage
       commit('SET_USER_DATA', DEFAULT_DATA)
       dispatch('SET_USER_DATA')
     }
   } catch (e) {
+    // Set default user data and save to localStorage
     console.error(e)
-    commit('SET_USER_DATA', {})
+    commit('SET_USER_DATA', DEFAULT_DATA)
+    dispatch('SET_USER_DATA')
   }
 }
 
 const SET_USER_DATA = ({ state }) => {
   try {
+    // Save current user data to localStorage
     localStorage.setItem(_VERSION, JSON.stringify(state.userData))
   } catch (e) {
+    // Save default data
     console.error(e)
-    localStorage.setItem(_VERSION, '{}')
+    localStorage.setItem(_VERSION, JSON.stringify(DEFAULT_DATA))
   }
 }
 
