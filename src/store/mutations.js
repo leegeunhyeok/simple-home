cc217f244b8f020fa78918a2716c3eabc8534340
leegeunhyeok <lghlove0509@naver.com>
2019-03-31 20:33:53 +0900

    UPDATE: mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,11 +1,18 @@
+
+const SET_WIDTH = (state, width) => {
+  state.screenWidth = width
+}
+
+const SET_SELECTED_MENU = (state, index) => {
+  state.selectedMenuIndex = index
+}
+
+const SET_STATE = (state, { key, value }) => {
+  state[key] = value
+}
+
 export default {
-  SET_WIDTH (state, width) {
-    state.screenWidth = width
-  },
-  SET_SELECTED_MENU (state, index) {
-    state.selectedMenuIndex = index
-  },
-  SET_STATE (state, { key, value }) {
-    state[key] = value
-  }
+  SET_WIDTH,
+  SET_SELECTED_MENU,
+  SET_STATE
 }
